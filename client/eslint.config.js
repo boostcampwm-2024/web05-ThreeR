19207824434afcd07c31a27b877d8d58c14335af
junyeokk <cjh4302@gmail.com>
2024-11-04 19:14:08 +0900

    📦 chore(config): project setup
    - ESLint, Prettier 수정

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -4,13 +4,13 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   ...tseslint.configs.recommended,
   {
     ...pluginReact.configs.flat.recommended,
     rules: {
-      'react/react-in-jsx-scope': 'off',
+      "react/react-in-jsx-scope": "off",
     },
   },
 ];
