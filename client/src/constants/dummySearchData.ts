bb102ad69e1f1da21ef7e1478f6837585774a47c
junyeokk <cjh4302@gmail.com>
2024-11-04 19:17:26 +0900

    ✨ feat(cards): post card, grid 컴포넌트 dummy data로 구현

---
+++ client/src/constants/dummySearchData.ts
@@ -0,0 +1,14 @@
+import { SearchData } from "@/types/search";
+
+export const SearchPost: SearchData[] = [
+  {
+    title: "Next.js 13 마이그레이션 가이드",
+    author: "토스 테크",
+    description: "Next.js 13이 출시되면서 많은 변화가 있었습니다...",
+  },
+  {
+    title: "Spring Boot 3.0 업데이트 내용",
+    author: "우아한형제들 기술블로그",
+    description: "Spring Boot 3.0에서는 Java 17이 기본이 되었으며...",
+  },
+];
