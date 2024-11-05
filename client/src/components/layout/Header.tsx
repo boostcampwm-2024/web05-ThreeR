4306bbb8cf67456c1a148898ab356364f5fa9f58
정명기 <jmk101711@naver.com>
2024-11-05 16:16:15 +0900

    ✨ feat: 검색버튼 컴포넌트 구현

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,9 +1,8 @@
 import logo from "@/assets/logo-denamu-main.svg";
-import searchIcon from "@/assets/searchIcon.svg";
 import SearchModal from "../search/SearchModal";
 import SideBar from "./Sidebar";
 import { useState } from "react";
-
+import SearchButton from "../search/SearchButton";
 export default function Header() {
   const [searchOpen, setSearchOpen] = useState<boolean>(false);
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
@@ -18,13 +17,16 @@ export default function Header() {
 
   return (
     <header className="mx-7 my-5 flex justify-between items-center h-[50px] relative">
-      <div className="cursor-pointer h-full">
+      <div
+        className="cursor-pointer h-full"
+        onClick={() => {
+          location.reload();
+        }}
+      >
         <img src={logo} alt="Logo" className="h-full" />
       </div>
       <div className="hidden md:flex h-full items-center gap-x-3 text-sm">
-        <button className="cursor-pointer border h-full px-4 rounded flex items-center" onClick={handleSearchModal}>
-          <img src={searchIcon} alt="Search" className="h-5" />
-        </button>
+        <SearchButton handleSearchModal={handleSearchModal} />
         <button className="cursor-pointer border h-full px-3 rounded flex items-center">로그인</button>
         <button className="cursor-pointer primary-div h-full px-3 rounded text-white font-bold flex items-center">
           블로그 등록
