3452e699792062177b24a73b3fe0541b3d4a0e3b
codeVac513 <xssdsw@naver.com>
2024-11-06 00:14:21 +0900

    ✨ feat: winston logger 구현

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,63 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { WinstonModule } from 'nest-winston';
+import { logDir, logFormat } from '../logger.config';
+import * as DailyRotateFile from 'winston-daily-rotate-file';
+import * as winston from 'winston';
+import * as process from 'node:process';
+
+const { combine, timestamp, label } = winston.format;
+
+// production 환경과 development 환경을 구분하기 위한 flag
+const productionFlag = process.env.NODE_ENV === 'production';
+
 @Module({
-  imports: [],
+  imports: [
+    WinstonModule.forRoot({
+      // 로그 출력 형식에 대한 정의
+      format: combine(
+        timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        label({ label: 'Denamu' }),
+        logFormat,
+      ),
+
+      transports: [
+        ...(productionFlag
+          ? [
+              //info 레벨 로그 파일 설정
+              new DailyRotateFile({
+                level: 'info',
+                datePattern: 'YYYY-MM-DD',
+                dirname: logDir,
+                filename: `%DATE%.log`,
+                maxFiles: 30,
+                zippedArchive: true,
+              }),
+              //error 레벨 로그 파일 설정
+              new DailyRotateFile({
+                level: 'error',
+                datePattern: 'YYYY-MM-DD',
+                dirname: `${logDir}/error`,
+                filename: `%DATE%.error.log`,
+                maxFiles: 30,
+                zippedArchive: true,
+              }),
+            ]
+          : [
+              // 콘솔에 로그 출력
+              new winston.transports.Console({
+                format: winston.format.combine(
+                  winston.format.colorize(),
+                  timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+                  label({ label: 'Denamu' }),
+                  logFormat,
+                ),
+              }),
+            ]),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
