3ee01de7ba301108cd3dea43d0e4f3893bb49e47
junyeokk <cjh4302@gmail.com>
2024-11-06 18:43:14 +0900

    ✨ feat: 무한 스크롤 커스텀 훅 구현 및 mock api 생성
    - 재사용을 위해 useInfiniteScroll 커스텀 훅 생성
    - 무한 스크롤 테스트를 위한 mock api 추가
    - LoadingIndicator 컴포넌트 구현
    - LatestSection 무한 스크롤 업데이트 - dummy data 제거

--- client/src/components/sections/LatestSection.tsx
+++ client/src/components/sections/LatestSection.tsx
@@ -1,14 +1,30 @@
+import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
 import { SectionHeader } from "@/components/common/SectionHeader";
 import { Rss } from "lucide-react";
 import { PostCardGrid } from "@/components/common/Card/PostCardGrid";
-import { LATEST_POSTS } from "@/constants/dummyData";
+import { Post } from "@/types/post";
+import { fetchPosts } from "@/services/mockApi";
+import { LoadingIndicator } from "../common/LoadingIndicator";
+
+const fetchPostsAdapter = async (page: number) => {
+  const response = await fetchPosts(page);
+  return {
+    data: response.posts,
+    hasMore: response.hasMore,
+  };
+};
 
 export default function LatestSection() {
+  const { items: posts, loading, observerTarget } = useInfiniteScroll<Post>({ fetchData: fetchPostsAdapter });
+
   return (
     <section className="flex flex-col p-4 min-h-[300px]">
       <SectionHeader icon={Rss} text="최신 포스트" description="최근에 작성된 포스트" iconColor="text-orange-500" />
       <div className="flex-1 mt-4 p-4 border border-dashed border-gray-500 rounded-lg">
-        <PostCardGrid posts={LATEST_POSTS} />
+        <PostCardGrid posts={posts} />
+        <div ref={observerTarget} className="h-10 flex items-center justify-center mt-4">
+          {loading && <LoadingIndicator />}
+        </div>
       </div>
     </section>
   );
