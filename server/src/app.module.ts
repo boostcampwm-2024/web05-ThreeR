fe4227b42fdf809f4618db7d2c7a6dcfea5122be
J_Coder <seok3765@naver.com>
2024-11-07 13:41:38 +0900

    ✨ feat: Test 환경 구축 DB Connect - sqlite3 - mysql

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
-
 import { winstonModule } from './common/logger/logger.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { loadDBSetting } from './common/database/load.config';
 
 @Module({
-  imports: [winstonModule],
+  imports: [
+    winstonModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${__dirname}/src/configs/.env.db.${process.env.NODE_ENV === 'test' ? 'test' : 'production'}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        loadDBSetting(configService),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ server/src/common/database/load.config.ts
@@ -0,0 +1,25 @@
+import { ConfigService } from '@nestjs/config';
+
+export function loadDBSetting(configService: ConfigService) {
+  const type = configService.get<'mysql' | 'sqlite'>('DB_TYPE');
+  const database = configService.get<string>('DB_DATABASE');
+  const host = configService.get<string | null>('DB_HOST');
+  const port = configService.get<number>('DB_PORT');
+  const username = configService.get<string>('DB_USERNAME');
+  const password = configService.get<string>('DB_PASSWORD');
+  const entities = [__dirname + '/../**/*.entity.{js,ts}'];
+  const synchronize = true;
+  const logging = process.env.NODE_ENV === 'production' ? false : true;
+
+  return {
+    type,
+    database,
+    host,
+    port,
+    username,
+    password,
+    entities,
+    synchronize,
+    logging,
+  };
+}
