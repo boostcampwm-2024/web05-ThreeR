2863c262d6eda1333c231cf6616c98879b471900
asn6878 <asn6878@gmail.com>
2024-11-10 01:32:30 +0900

    🐛 fix: rss 조회로직 connection release 처리

--- rss-notifier/src/db-access.ts
+++ rss-notifier/src/db-access.ts
@@ -1,6 +1,6 @@
 import logger from "./logger.js";
 import { FeedObj, FeedDetail } from "./types.js";
-import 'dotenv/config';
+import "dotenv/config";
 import * as mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
@@ -12,32 +12,31 @@ export const pool = mysql.createPool({
 
 export const executeQuery = async (query: string, params: any[] = []) => {
   let connection;
-  connection = await pool.getConnection();
-  const [rows] = await connection.query(query, params);
-
-  connection.release();
-  return rows;
-};
-
-// 모든 RSS 데이터를 선택하는 함수
-export const selectAllRss = async (): Promise<FeedObj[]> => {
-  const query = `SELECT id, rss_url FROM blog`;
   try {
-    return await executeQuery(query);
+    connection = await pool.getConnection();
+    const [rows] = await connection.query(query, params);
+    return rows;
   } catch (err) {
     logger.error("쿼리 " + query + " 실행 중 오류 발생:", err);
-    return [];
+    throw err;
+  } finally {
+    if (connection) {
+      connection.release();
+    }
   }
 };
 
-// feed 데이터들을 받아 DB에 저장하는 함수
+export const selectAllRss = async (): Promise<FeedObj[]> => {
+  const query = `SELECT id, rss_url FROM blog`;
+  return executeQuery(query);
+};
+
 export const insertFeeds = async (resultData: FeedDetail[]) => {
   const query = `
     INSERT INTO feed (blog_id, created_at, title, path, thumbnail)
     VALUES ?
   `;
 
-  // 여러 행의 값을 배열 형식으로 준비
   const values = resultData.map((feed) => [
     feed.blog_id,
     feed.pub_date,
@@ -46,23 +45,9 @@ export const insertFeeds = async (resultData: FeedDetail[]) => {
     feed.imageUrl,
   ]);
 
-  let connection;
-  try {
-    connection = await pool.getConnection();
-    await connection.beginTransaction();
+  await executeQuery(query, [values]);
 
-    await executeQuery(query, [values]);
-    await connection.commit();
-
-    logger.info(
-      `${resultData.length}개의 피드 데이터가 성공적으로 삽입되었습니다.`
-    );
-  } catch (err) {
-    if (connection) await connection.rollback();
-    logger.error("쿼리 " + query + " 실행 중 오류 발생:", err);
-  } finally {
-    if (connection) {
-      connection.release();
-    }
-  }
+  logger.info(
+    `${resultData.length}개의 피드 데이터가 성공적으로 삽입되었습니다.`
+  );
 };
