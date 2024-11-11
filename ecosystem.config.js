8d5bde5cedc315cfdaf0ba43f634edee98046c57
asn6878 <asn6878@gmail.com>
2024-11-12 01:37:01 +0900

    ✨ feat: pm2 환경변수 설정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   apps: [
     {
@@ -5,11 +7,11 @@ module.exports = {
       script: "./server/dist/main.js",
       instances: "2",
       exec_mode: "cluster",
+      watch: false,
+      autorestart: true,
       env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
         NODE_ENV: "production",
+        ENV_PATH: "server/configs/.env.db.production",
       },
     },
     {
@@ -17,7 +19,7 @@ module.exports = {
       script: "./rss-notifier/dist/main.js",
       instances: "1",
       exec_mode: "fork",
-      cron_restart: `0 * * * *`,
+      cron_restart: `*/30 * * * *`, // 30분 마다 rss-notifier 재시작
       autorestart: false,
       watch: false,
     },
