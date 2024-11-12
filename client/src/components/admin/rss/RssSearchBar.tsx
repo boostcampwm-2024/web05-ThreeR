6f9624b911824372b7612c48f19a569d914513f6
junyeokk <cjh4302@gmail.com>
2024-11-12 09:55:02 +0900

    ✨ feat: rSS 요청 관리 추가
    - RSS 요청 card 컴포넌트 추가
    - 요청 거부 모달 구현
    - RSS 요청 검색 컴포넌트 구현

---
+++ client/src/components/admin/rss/RssSearchBar.tsx
@@ -0,0 +1,16 @@
+import { Search } from "lucide-react";
+
+import { Input } from "@/components/ui/input";
+
+export const RssRequestSearchBar = () => {
+  return (
+    <>
+      <div className="mb-8 flex space-x-4">
+        <div className="relative flex-1">
+          <Search className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
+          <Input placeholder="블로그명, URL 또는 신청자로 검색" className="pl-10" />
+        </div>
+      </div>
+    </>
+  );
+};
