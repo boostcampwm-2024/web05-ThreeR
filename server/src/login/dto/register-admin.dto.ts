63af126f83cade7e5760e0196b9461e4e8738970
codeVac513 <xssdsw@naver.com>
2024-11-12 17:29:37 +0900

    ♻️ refactor: 검증 로직을 Service 계층에서 분리하여 DTO에서 검증하도록 수정

--- server/src/login/dto/register-admin.dto.ts
+++ server/src/login/dto/register-admin.dto.ts
@@ -1,9 +1,14 @@
-import { IsString } from 'class-validator';
+import { IsString, Length, Matches } from 'class-validator';
+
+const PASSWORD_REG = /^(?=.*[!@#$%^&*()_+])[A-Za-z0-9!@#$%^&*()_+]+$/;
 
 export class RegisterAdminDto {
   @IsString()
+  @Length(6, 255)
   loginId: string;
 
   @IsString()
+  @Matches(PASSWORD_REG)
+  @Length(6, 60)
   password: string;
 }
