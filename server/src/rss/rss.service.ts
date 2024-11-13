3e4bc39eb13b816aa8f4588b86b49dfb1399bc86
J_Coder <seok3765@naver.com>
2024-11-13 23:01:24 +0900

    ✨ feat: rss 등록 service 구현

--- server/src/rss/rss.service.ts
+++ server/src/rss/rss.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { RssRepository } from './rss.repository';
+import { RssRegisterDto } from './dto/rss-register.dto';
 
 @Injectable()
-export class RssService {}
+export class RssService {
+  constructor(private readonly rssRepository: RssRepository) {}
+
+  async registerRss(rssRegisterDto: RssRegisterDto) {
+    const alreadyURL = await this.rssRepository.findOne({
+      where: {
+        rssURL: rssRegisterDto.rssURL,
+      },
+    });
+
+    if (alreadyURL) {
+      throw new ConflictException('이미 등록된 RSS URL입니다.');
+    }
+
+    await this.rssRepository.insertNewRss(rssRegisterDto);
+  }
+}
