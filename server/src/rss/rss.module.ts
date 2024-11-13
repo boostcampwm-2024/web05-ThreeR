75bf4b9cab0d5da55cd18ceb27e15f3fec9792d6
asn6878 <asn6878@gmail.com>
2024-11-14 02:46:25 +0900

    🐛 fix: RssRepository 사용하도록 변경

--- server/src/rss/rss.module.ts
+++ server/src/rss/rss.module.ts
@@ -7,8 +7,8 @@ import { RssRepository } from './rss.repository';
 import { BlogRepository } from '../blog/blog.repository';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([RssInformation])],
-    controllers: [RssController],
-    providers: [RssService, RssRepository, BlogRepository],
+  imports: [TypeOrmModule.forFeature([RssInformation])],
+  controllers: [RssController],
+  providers: [RssService, RssRepository, BlogRepository],
 })
 export class RssModule {}
