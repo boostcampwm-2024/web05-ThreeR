66980d0b82c25e58111258ccc0530b80149adeaf
jungmyunggi <143400940+jungmyunggi@users.noreply.github.com>
2024-11-15 01:46:48 +0900

    ✨ feat: rss등록 hook구현

--- client/src/api/instance.ts
+++ client/src/api/instance.ts
@@ -4,3 +4,8 @@ export const api = axios.create({
   baseURL: "/api",
   timeout: 10000,
 });
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 10000,
+});
