211d46551113f571b78ae00f7f08dfdc00689697
J_Coder <seok3765@naver.com>
2024-11-16 23:31:55 +0900

    ♻️ refactor: hTTP Exception Filter, Internal Exception Filter 분리

---
+++ server/src/common/filters/http-exception.filter.ts
@@ -0,0 +1,26 @@
+// src/common/filters/all-exceptions.filter.ts
+
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { ApiResponse } from '../response/common.response';
+import { WinstonLoggerService } from '../logger/logger.service';
+
+@Catch(HttpException)
+export class HttpExceptionsFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const response = host.switchToHttp().getResponse<Response>();
+
+    const statusCode = exception.getStatus();
+    const res = exception.getResponse();
+    const message = typeof res === 'string' ? res : res['message'];
+
+    const apiResponse = ApiResponse.responseWithNoContent(message);
+    response.status(statusCode).json(apiResponse);
+  }
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,18 +1,22 @@
+import { HttpExceptionsFilter } from './common/filters/http-exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './common/swagger/swagger';
 import * as cookieParser from 'cookie-parser';
-import { GlobalExceptionsFilter } from './common/filters/global-exceptions.filter';
+import { InternalExceptionsFilter } from './common/filters/internal-exceptions.filter';
 import { LoggingInterceptor } from './common/logger/logger.interceptor';
 import { WinstonLoggerService } from './common/logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = app.get(WinstonLoggerService);
+  app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.useGlobalInterceptors(new LoggingInterceptor(logger));
-  app.setGlobalPrefix('api');
-  app.useGlobalFilters(new GlobalExceptionsFilter());
+  app.useGlobalFilters(
+    new InternalExceptionsFilter(logger),
+    new HttpExceptionsFilter(),
+  );
   app.enableCors({
     origin: ['http://localhost:5173', 'https://denamu.netlify.app/'],
     credentials: true,
