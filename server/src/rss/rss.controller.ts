863134ea5310974fa7e8db7a7dec35f4ad3a0339
J_Coder <seok3765@naver.com>
2024-11-16 22:00:28 +0900

    ♻️ refactor: 도메인 별 로거에서 전역 로거로 변경

--- server/src/rss/rss.controller.ts
+++ server/src/rss/rss.controller.ts
@@ -3,33 +3,25 @@ import {
   Controller,
   Get,
   HttpCode,
-  Inject,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { RssService } from './rss.service';
-import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { RssRegisterDto } from './dto/rss-register.dto';
 import { ApiPostRegisterRss, ApiGetRss } from './rss.api-docs';
-import { Logger } from 'winston';
 import { ApiResponse } from '../common/response/common.response';
 
 @ApiTags('RSS')
 @Controller('rss')
 export class RssController {
-  constructor(
-    private readonly rssService: RssService,
-    @Inject(WINSTON_MODULE_PROVIDER)
-    private readonly logger: Logger,
-  ) {}
+  constructor(private readonly rssService: RssService) {}
 
   @ApiPostRegisterRss()
   @Post()
   @UsePipes(ValidationPipe)
   async postRegisterRss(@Body() rssRegisterDto: RssRegisterDto) {
-    this.logger.info(JSON.stringify(rssRegisterDto));
     await this.rssService.registerRss(rssRegisterDto);
     return ApiResponse.responseWithNoContent('신청이 완료되었습니다.');
   }
