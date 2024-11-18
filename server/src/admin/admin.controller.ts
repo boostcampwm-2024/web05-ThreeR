bf9c80278ffd1fc4de5af7a6fe4b5193bc408bc1
codeVac513 <xssdsw@naver.com>
2024-11-18 09:16:22 +0900

    🧼 clean: winston logger업데이트에 따라 사용하지 않는 로깅 코드 정리

--- server/src/admin/admin.controller.ts
+++ server/src/admin/admin.controller.ts
@@ -3,7 +3,6 @@ import {
   Controller,
   HttpCode,
   HttpStatus,
-  Inject,
   Post,
   Res,
   UsePipes,
@@ -11,8 +10,6 @@ import {
 } from '@nestjs/common';
 import { Response } from 'express';
 import { AdminService } from './admin.service';
-import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
-import { Logger } from 'winston';
 import { RegisterAdminDto } from './dto/register-admin.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { ApiPostLoginAdmin, ApiPostRegisterAdmin } from './admin.api-docs';
@@ -22,10 +19,7 @@ import { LoginAdminDto } from './dto/login-admin.dto';
 @ApiTags('Admin')
 @Controller('admin')
 export class AdminController {
-  constructor(
-    private readonly loginService: AdminService,
-    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
-  ) {}
+  constructor(private readonly loginService: AdminService) {}
 
   @ApiPostLoginAdmin()
   @Post('/login')
@@ -36,8 +30,6 @@ export class AdminController {
     @Res({ passthrough: true }) response: Response,
   ) {
     await this.loginService.loginAdmin(loginAdminDto, response);
-    this.logger.info(`admin 로그인: ${loginAdminDto.loginId}`);
-
     return ApiResponse.responseWithNoContent(
       '로그인이 성공적으로 처리되었습니다.',
     );
@@ -48,7 +40,6 @@ export class AdminController {
   @UsePipes(ValidationPipe)
   async registerAdmin(@Body() registerAdminDto: RegisterAdminDto) {
     await this.loginService.registerAdmin(registerAdminDto);
-    this.logger.info(`admin 등록: ${registerAdminDto.loginId}`);
     return ApiResponse.responseWithNoContent(
       '성공적으로 관리자 계정이 생성되었습니다.',
     );
--- server/src/feed/feed.controller.ts
+++ server/src/feed/feed.controller.ts
@@ -5,32 +5,24 @@ import {
   Get,
   HttpCode,
   HttpStatus,
-  Inject,
   Query,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { FeedService } from './feed.service';
-import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
-import { Logger } from 'winston';
 import { QueryFeedDto } from './dto/query-feed.dto';
 import { ApiGetFeedList } from './feed.api-docs';
 
 @ApiTags('Feed')
 @Controller('feed')
 export class FeedController {
-  constructor(
-    private readonly feedService: FeedService,
-    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
-  ) {}
+  constructor(private readonly feedService: FeedService) {}
 
   @ApiGetFeedList()
   @Get('')
   @HttpCode(HttpStatus.OK)
   @UsePipes(ValidationPipe)
   async getFeedList(@Query() queryFeedDto: QueryFeedDto) {
-    this.logger.info(JSON.stringify(queryFeedDto));
-
     const feedList = await this.feedService.getFeedList(queryFeedDto);
     const hasMore = this.feedService.existNextFeed(
       feedList,
@@ -38,9 +30,6 @@ export class FeedController {
     );
     const lastId = this.feedService.getLastIdFromFeedList(feedList);
     const data = { result: feedList, lastId, hasMore };
-
-    this.logger.info(`lastId: ${lastId}`);
-
     return ApiResponse.responseWithData('피드 조회 완료', data);
   }
 }
