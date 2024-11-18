753cbe02b28b43d5f9c36a5a72a336612c981270
junyeokk <cjh4302@gmail.com>
2024-11-19 00:27:24 +0900

    🐛 fix: posts API 요청, 응답 값 백엔드 명세서에 맞게 수정
    - 엔드포인트 /api/feed로 변경
    - 파라미터 구조 last_id -> lastId
    - 응답 데이터 구조 API 스펙하고 맞출 수 있 변경

--- client/src/api/posts.ts
+++ client/src/api/posts.ts
@@ -2,12 +2,17 @@ import { api } from "@/api/instance";
 import { InfiniteScrollResponse, PostsApiResponse, Post } from "@/types/post";
 
 export const postsApi = {
-  fetchPosts: async (params: { limit: number; last_id?: number }): Promise<InfiniteScrollResponse<Post>> => {
-    const response = await api.get<PostsApiResponse>("/posts", { params });
+  fetchPosts: async (params: { limit: number; last_id: number }): Promise<InfiniteScrollResponse<Post>> => {
+    const response = await api.get<PostsApiResponse>("/api/feed", {
+      params: {
+        limit: params.limit,
+        lastId: params.last_id || 0,
+      },
+    });
     return {
-      posts: response.data.data.result,
-      hasMore: response.data.data.last_id !== null,
-      nextPage: response.data.data.last_id,
+      result: response.data.data.result,
+      hasMore: response.data.data.hasMore,
+      lastId: response.data.data.lastId,
     };
   },
 };
