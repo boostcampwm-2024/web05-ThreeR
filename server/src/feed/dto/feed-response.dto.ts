8eaedabde2f2e144c83b2efd8cad5f7a8ff7dd8e
codeVac513 <xssdsw@naver.com>
2024-11-19 16:49:40 +0900

    🧼 clean: 사용하지 않는 데코레이터 삭제

--- server/src/feed/dto/feed-response.dto.ts
+++ server/src/feed/dto/feed-response.dto.ts
@@ -1,48 +1,18 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { Feed } from '../feed.entity';
 
 export class FeedResponseDto {
-  @ApiProperty({
-    example: 1,
-    description: '피드 ID',
-  })
   id: number;
 
-  @ApiProperty({
-    example: '박무성',
-    description: '작성자 이름',
-  })
   author: string;
 
-  @ApiProperty({
-    example: 'ECS vs EKS 비교 분석',
-    description: '게시물 제목',
-  })
   title: string;
 
-  @ApiProperty({
-    example: 'https://dev-park.dev/ecs-eks',
-    description: '피드 URL',
-  })
   path: string;
 
-  @ApiProperty({
-    example: '2024-03-19T12:00:00Z',
-    description: '작성일',
-  })
   createAt: Date;
 
-  @ApiProperty({
-    example: 'https://dev-park.dev/thumbnails/containers.jpg',
-    description: '썸네일 이미지 URL',
-    required: false,
-  })
   thumbnail: string;
 
-  @ApiProperty({
-    example: 100,
-    description: '조회수',
-  })
   viewCount: number;
 
   private constructor(
