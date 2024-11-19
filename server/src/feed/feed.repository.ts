fc6996a0201cd9fcbb674864978f1f79fa7c59ac
codeVac513 <xssdsw@naver.com>
2024-11-19 14:19:45 +0900

    🐛 fix: feed 요청 시 author 프로퍼티가 추가되도록 수정

--- server/src/feed/feed.repository.ts
+++ server/src/feed/feed.repository.ts
@@ -1,20 +1,23 @@
-import { DataSource, Repository } from 'typeorm';
+import { DataSource, LessThan, Repository } from 'typeorm';
 import { Feed } from './feed.entity';
 import { Injectable } from '@nestjs/common';
-import { QueryFeedDto } from './dto/query-feed.dto';
 
 @Injectable()
 export class FeedRepository extends Repository<Feed> {
   constructor(private dataSource: DataSource) {
     super(Feed, dataSource.createEntityManager());
   }
+
   async findFeed(queryFeedDto: QueryFeedDto) {
     const { lastId, limit } = queryFeedDto;
 
-    return await this.createQueryBuilder('feed')
-      .where(lastId ? `feed.id < ${lastId}` : ``)
-      .limit(limit + 1)
-      .addOrderBy('feed.id', 'DESC')
-      .getMany();
+    const whereCondition: any = lastId ? { id: LessThan(lastId) } : {};
+    return await this.find({
+      where: whereCondition,
+      order: { id: 'DESC' },
+      take: limit + 1,
+    });
   }
 }
+
+import { QueryFeedDto } from './dto/query-feed.dto';
--- server/src/feed/feed.service.ts
+++ server/src/feed/feed.service.ts
@@ -3,25 +3,52 @@ import { FeedRepository } from './feed.repository';
 import { QueryFeedDto } from './dto/query-feed.dto';
 import { Feed } from './feed.entity';
 
+type Post = {
+  id: number;
+  author: string;
+  title: string;
+  path: string;
+  createAt: Date;
+  thumbnail: string;
+  viewCount: number;
+};
+
 @Injectable()
 export class FeedService {
   constructor(private readonly feedRepository: FeedRepository) {}
 
-  async getFeedData(queryFeedDto: QueryFeedDto) {
-    const result = await this.feedRepository.findFeed(queryFeedDto);
+  async getPostData(queryFeedDto: QueryFeedDto) {
+    const feedList = await this.feedRepository.findFeed(queryFeedDto);
+    const result = this.mapFeedsToPost(feedList);
     const hasMore = this.existNextFeed(result, queryFeedDto.limit);
     if (hasMore) result.pop();
     const lastId = this.getLastIdFromFeedList(result);
     return { result, lastId, hasMore };
   }
 
-  private existNextFeed(feedList: Feed[], limit: number) {
-    return feedList.length > limit;
+  private existNextFeed(postList: Post[], limit: number) {
+    return postList.length > limit;
   }
 
-  private getLastIdFromFeedList(feedList: Feed[]) {
-    if (feedList.length === 0) return 0;
-    const lastFeed = feedList[feedList.length - 1];
+  private getLastIdFromFeedList(postList: Post[]) {
+    if (postList.length === 0) return 0;
+    const lastFeed = postList[postList.length - 1];
     return lastFeed.id;
+  }
+
+  private mapFeedToPost(feed: Feed): Post {
+    return {
+      id: feed.id,
+      author: feed.blog.userName,
+      title: feed.title,
+      path: feed.path,
+      createAt: feed.createdAt,
+      thumbnail: feed.thumbnail,
+      viewCount: feed.viewCount,
+    };
+  }
+
+  private mapFeedsToPost(FeedList: Feed[]) {
+    return FeedList.map(this.mapFeedToPost);
   }
 }
