526e6186926d7621fe171bc922b03c8716e7c8c4
jungmyunggi <143400940+jungmyunggi@users.noreply.github.com>
2024-11-19 17:50:05 +0900

    ♻️ refactor: rss등록 관리 로직 내부 컴포넌트로 이전

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -2,41 +2,15 @@ import { useState } from "react";
 
 import { AdminHeader } from "@/components/admin/layout/AdminHeader";
 import AdminLogin from "@/components/admin/login/AdminLoginModal";
-import { RejectModal } from "@/components/admin/rss/RejectModal";
 import { RssRequestSearchBar } from "@/components/admin/rss/RssSearchBar";
 
-import { useAdminAccept } from "@/hooks/useRssActions";
-
 import { AdminTabs } from "../components/admin/layout/AdminTabs";
-import { AdminRssData, AdminRequest } from "@/types/rss";
 
 export default function Admin() {
-  const [selectedBlogName, setSelectedBlogName] = useState<string>();
   const [isLogin, setIsLogin] = useState<boolean>(false);
 
   const handleLogin = () => {
     setIsLogin((prev) => !prev);
-  };
-
-  const onSuccess = () => {
-    console.log();
-  };
-
-  const onError = (error: any) => {
-    console.log(error);
-  };
-  const { mutate } = useAdminAccept(onSuccess, onError);
-
-  const handleAccept = (data: AdminRequest) => {
-    mutate(data);
-  };
-
-  const handleAction = (request: AdminRssData, action: "approve" | "reject", data: AdminRequest) => {
-    if (action === "reject") {
-      setSelectedBlogName(request.name);
-    } else {
-      handleAccept(data);
-    }
   };
 
   const renderElement = {
@@ -45,7 +19,7 @@ export default function Admin() {
         <AdminHeader setLogin={handleLogin} />
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <RssRequestSearchBar />
-          <AdminTabs onAction={handleAction} />
+          <AdminTabs />
         </div>
       </div>
     ),
