25db6d7d53eba6b519792264022c4af776b4ba1c
jungmyunggi <143400940+jungmyunggi@users.noreply.github.com>
2024-11-20 01:07:55 +0900

    ✨ feat: shadcn ScrollArea 추가

--- client/src/components/chat/layout/ChatSection.tsx
+++ client/src/components/chat/layout/ChatSection.tsx
@@ -1,33 +1,16 @@
 import ChatItem from "@/components/chat/ChatItem";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
-type ChatType = {
-  chatImg: string;
-  chatName: string;
-  chatTime: string;
-  chatContent: string;
-};
-
-const CHAT_ITEM: ChatType[] = [
-  {
-    chatImg: "https://github.com/shadcn.png",
-    chatName: "John Doe",
-    chatTime: "10:30 AM",
-    chatContent: "Hello, how are you?",
-  },
-  {
-    chatImg: "https://github.com/shadcn.png",
-    chatName: "Jane Doe",
-    chatTime: "11:00 AM",
-    chatContent: "I'm good, thank you!",
-  },
-];
+import { CHAT_ITEM } from "@/constants/dummyData";
 
 export default function ChatSection() {
   return (
-    <ul className="flex flex-col gap-5 grow">
-      {CHAT_ITEM.map((item) => {
-        return <ChatItem {...item} />;
-      })}
-    </ul>
+    <ScrollArea>
+      <ul className="flex flex-col gap-5 grow">
+        {CHAT_ITEM.map((item) => {
+          return <ChatItem {...item} />;
+        })}
+      </ul>
+    </ScrollArea>
   );
 }
