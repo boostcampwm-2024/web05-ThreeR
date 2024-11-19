dbf78895eebadd00224455d877d3c622a683df6e
junyeokk <cjh4302@gmail.com>
2024-11-19 15:17:35 +0900

    ♻️ refactor: axios 인스턴스 분리
    - api 인스턴스의 baseURL을 /api로 변경해 mock API 용으로 설정
    - axiosInstance 추가해 실제 API 호출용으로 설정

--- client/src/api/instance.ts
+++ client/src/api/instance.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
+  baseURL: "/api",
+  timeout: 10000,
+});
+
+export const axiosInstance = axios.create({
   baseURL: "https://api.denamu.shop",
-  headers: {
-    "Content-Type": "application/json",
-  },
   timeout: 10000,
 });
