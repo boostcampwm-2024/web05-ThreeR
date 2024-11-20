582a06a1f30592b0f51d73ac06b29cfbce821757
junyeokk <cjh4302@gmail.com>
2024-11-20 11:37:12 +0900

    ♻️ refactor: mock 데이터도 바뀐 타입에 맞게 변경

--- client/src/api/mocks/data/posts.ts
+++ client/src/api/mocks/data/posts.ts
@@ -4,10 +4,12 @@ const PAGE_SIZE = 12;
 
 export const generateMockPost = (id: number): Post => ({
   id: id,
-  title: `블로그 포스트 #${id}`,
-  author: `작성자 ${(id % 5) + 1}`,
-  thumbnailUrl: `https://picsum.photos/640/480?random=${id}`,
   createdAt: new Date(Date.now() - id * 86400000).toISOString(),
+  title: `블로그 포스트 #${id}`,
+  viewCount: 0,
+  path: "/",
+  author: `작성자 ${(id % 5) + 1}`,
+  thumbnail: `https://picsum.photos/640/480?random=${id}`,
 });
 
 export const TOTAL_POSTS = Array.from({ length: 100 }, (_, i) => generateMockPost(i + 1));
