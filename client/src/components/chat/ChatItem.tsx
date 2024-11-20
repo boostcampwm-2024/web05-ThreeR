add55705afec71bea9b99f36ceae3df44035c8a8
jungmyunggi <143400940+jungmyunggi@users.noreply.github.com>
2024-11-20 10:52:01 +0900

    ✨ feat: 본인 메시지 다른스타일로 노출

--- client/src/components/chat/ChatItem.tsx
+++ client/src/components/chat/ChatItem.tsx
@@ -1,31 +1,40 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 
-type ChatType = {
-  chatImg: string;
-  chatName: string;
-  chatTime: string;
-  chatContent: string;
-};
+import { ChatType } from "@/types/chat";
 
 export default function ChatItem(chatItem: ChatType) {
+  const isMyMessage = !chatItem.chatOther;
+
   return (
-    <div className="chat-item flex gap-5">
-      {/* 이미지 */}
-      <Avatar>
-        <AvatarImage src={chatItem.chatImg} />
-      </Avatar>
-      <div className="flex flex-col gap-1">
+    <div className={`flex gap-2 ${isMyMessage ? "justify-end" : "justify-start"} items-center`}>
+      {!isMyMessage && (
+        <Avatar>
+          <AvatarImage src={chatItem.chatImg} />
+        </Avatar>
+      )}
+
+      <div className={`flex flex-col gap-1 ${isMyMessage ? "items-end text-right" : "items-start text-left"}`}>
         {/* 이름, 시간 */}
         <div className="flex gap-2 items-end">
-          <span className="text-sm">{chatItem.chatName}</span>
+          {!isMyMessage && <span className="text-sm">{chatItem.chatName}</span>}
           <span className="text-xs">{chatItem.chatTime}</span>
         </div>
+
         {/* 채팅 내용 */}
-        <Badge variant="secondary" className="p-3">
+        <Badge
+          variant="secondary"
+          className={`p-3 ${isMyMessage ? "bg-blue-500 text-white" : "bg-gray-200 text-black"}`}
+        >
           {chatItem.chatContent}
         </Badge>
       </div>
+
+      {isMyMessage && (
+        <Avatar>
+          <AvatarImage src={chatItem.chatImg} />
+        </Avatar>
+      )}
     </div>
   );
 }
