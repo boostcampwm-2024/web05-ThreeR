298d4a8da2d4e0e5b566d09904bb7fa710940363
junyeokk <cjh4302@gmail.com>
2024-11-22 00:46:22 +0900

    ♻️ refactor: 게시물 카드 클릭 핸들러 로직 분리
    - handleClick -> handlePostClick으로 핸들러 이름 변경
    - 조회수 증가 기능 통합을 위한 구조 개선
    - 기존 window.open 호출을 커스텀 훅 이동

--- client/src/components/common/Card/PostCard.tsx
+++ client/src/components/common/Card/PostCard.tsx
@@ -1,5 +1,7 @@
 import { Card } from "@/components/ui/card";
 
+import { usePostCardActions } from "@/hooks/common/usePostCardActions";
+
 import { PostCardContent } from "./PostCardContent";
 import { PostCardImage } from "./PostCardImage";
 import { cn } from "@/lib/utils";
@@ -11,15 +13,11 @@ interface PostCardProps {
 }
 
 export const PostCard = ({ post, className }: PostCardProps) => {
-  const handleClick = () => {
-    if (post.path) {
-      window.open(post.path, "_blank", "noopener,noreferrer");
-    }
-  };
+  const { handlePostClick } = usePostCardActions(post);
 
   return (
     <Card
-      onClick={handleClick}
+      onClick={handlePostClick}
       className={cn(
         "group shadow-sm hover:shadow-xl transition-all duration-300 hover:-translate-y-0.5 border-none rounded-xl cursor-pointer",
         className
