dd8d5ff48c4e08e7525952ea4108e38033a52186
J_Coder <seok3765@naver.com>
2024-11-24 22:40:58 +0900

    📝 docs: trend 조회 API Deprecated, Dummy Data로 변경, schema 형식 작성

--- server/src/feed/feed.api-docs.ts
+++ server/src/feed/feed.api-docs.ts
@@ -153,66 +153,6 @@ export function ApiSearchFeed() {
   );
 }
 
-export function ApiGetTrendList() {
-  return applyDecorators(
-    ApiOperation({
-      summary: '트랜드 게시글 조회 API',
-    }),
-    ApiOkResponse({
-      description: 'Ok',
-      schema: {
-        example: {
-          message: '트렌드 피드 조회 완료',
-          data: [
-            {
-              id: 1,
-              author: '해야지 뭐',
-              title:
-                '자바스크립트의 구조와 실행 방식 (Ignition, TurboFan, EventLoop)',
-              path: 'https://asn6878.tistory.com/9',
-              createdAt: '2022-09-05 09:00:00',
-              thumbnail:
-                'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F2wH52%2FbtsJIskiFgS%2FQlF4XqMVZsM8y51w67dxj1%2Fimg.png',
-              viewCount: 0,
-            },
-            {
-              id: 2,
-              author: 'seok3765.log',
-              title:
-                '[네이버 커넥트재단 부스트캠프 웹・모바일 9기] 날 것 그대로 작성하는 챌린지 수료 후기 - Web',
-              path: 'https://velog.io/@seok3765/%EB%84%A4%EC%9D%B4%EB%B2%84-%EC%BB%A4%EB%84%A5%ED%8A%B8%EC%9E%AC%EB%8B%A8-%EB%B6%80%EC%8A%A4%ED%8A%B8%EC%BA%A0%ED%94%84-%EC%9B%B9%E3%83%BB%EB%AA%A8%EB%B0%94%EC%9D%BC-9%EA%B8%B0-%EB%82%A0-%EA%B2%83-%EA%B7%B8%EB%8C%80%EB%A1%9C-%EC%9E%91%EC%84%B1%ED%95%98%EB%8A%94-%EC%B1%8C%EB%A6%B0%EC%A7%80-%EC%88%98%EB%A3%8C-%ED%9B%84%EA%B8%B0-Web',
-              createdAt: '2024-08-14 14:07:49',
-              thumbnail:
-                'https://velog.velcdn.com/images/seok3765/post/2f863481-b594-46f8-9a28-7799afb58aa4/image.jpg',
-              viewCount: 0,
-            },
-            {
-              id: 3,
-              author: '해야지 뭐',
-              title: '제목',
-              path: 'https://asn6878.tistory.com/9',
-              createdAt: '2022-09-05 09:00:00',
-              thumbnail:
-                'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F2wH52%2FbtsJIskiFgS%2FQlF4XqMVZsM8y51w67dxj1%2Fimg.png',
-              viewCount: 0,
-            },
-            {
-              id: 4,
-              author: '해야지 뭐',
-              title: '제목',
-              path: 'https://asn6878.tistory.com/9',
-              createdAt: '2022-09-05 10:00:00',
-              thumbnail:
-                'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F2wH52%2FbtsJIskiFgS%2FQlF4XqMVZsM8y51w67dxj1%2Fimg.png',
-              viewCount: 0,
-            },
-          ],
-        },
-      },
-    }),
-  );
-}
-
 export function ApiGetTrendSse() {
   return applyDecorators(
     ApiOperation({
@@ -221,54 +161,82 @@ export function ApiGetTrendSse() {
     ApiOkResponse({
       description: 'Ok',
       schema: {
-        example: {
-          message: '트렌드 피드 수신 완료',
-          data: [
-            {
-              id: 1,
-              author: '해야지 뭐',
-              title:
-                '자바스크립트의 구조와 실행 방식1 (Ignition, TurboFan, EventLoop)',
-              path: 'https://asn6878.tistory.com/9',
-              createdAt: '2022-09-05 09:00:00',
-              thumbnail:
-                'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F2wH52%2FbtsJIskiFgS%2FQlF4XqMVZsM8y51w67dxj1%2Fimg.png',
-              viewCount: 0,
+        properties: {
+          message: {
+            type: 'string',
+          },
+          data: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                id: { type: 'number' },
+                author: { type: 'string' },
+                title: { type: 'string' },
+                path: { type: 'string' },
+                createdAt: {
+                  type: 'string',
+                  format: 'date-time',
+                },
+                thumbnail: { type: 'string' },
+                viewCount: { type: 'number' },
+              },
             },
-            {
-              id: 2,
-              author: 'seok3765.log',
-              title:
-                '[네이버 커넥트재단 부스트캠프 웹・모바일 9기] 날 것 그대로 작성하는 챌린지 수료 후기 - Web',
-              path: 'https://velog.io/@seok3765/%EB%84%A4%EC%9D%B4%EB%B2%84-%EC%BB%A4%EB%84%A5%ED%8A%B8%EC%9E%AC%EB%8B%A8-%EB%B6%80%EC%8A%A4%ED%8A%B8%EC%BA%A0%ED%94%84-%EC%9B%B9%E3%83%BB%EB%AA%A8%EB%B0%94%EC%9D%BC-9%EA%B8%B0-%EB%82%A0-%EA%B2%83-%EA%B7%B8%EB%8C%80%EB%A1%9C-%EC%9E%91%EC%84%B1%ED%95%98%EB%8A%94-%EC%B1%8C%EB%A6%B0%EC%A7%80-%EC%88%98%EB%A3%8C-%ED%9B%84%EA%B8%B0-Web',
-              createdAt: '2024-08-14 14:07:49',
-              thumbnail:
-                'https://velog.velcdn.com/images/seok3765/post/2f863481-b594-46f8-9a28-7799afb58aa4/image.jpg',
-              viewCount: 0,
-            },
-            {
-              id: 3,
-              author: '해야지 뭐',
-              title:
-                '자바스크립트의 구조와 실행 방식2 (Ignition, TurboFan, EventLoop)',
-              path: 'https://asn6878.tistory.com/10',
-              createdAt: '2022-09-05 09:00:00',
-              thumbnail:
-                'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F2wH52%2FbtsJIskiFgS%2FQlF4XqMVZsM8y51w67dxj1%2Fimg.png',
-              viewCount: 0,
-            },
-            {
-              id: 4,
-              author: '해야지 뭐',
-              title:
-                '자바스크립트의 구조와 실행 방식3 (Ignition, TurboFan, EventLoop)',
-              path: 'https://asn6878.tistory.com/11',
-              createdAt: '2022-09-05 10:00:00',
-              thumbnail:
-                'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F2wH52%2FbtsJIskiFgS%2FQlF4XqMVZsM8y51w67dxj1%2Fimg.png',
-              viewCount: 0,
-            },
-          ],
+          },
+        },
+      },
+      examples: {
+        connect: {
+          summary: '현재 트렌드 피드 수신 완료',
+          value: {
+            message: '현재 트렌드 피드 수신 완료',
+            data: [
+              {
+                id: 1,
+                author: '블로그 이름',
+                title: '게시글 제목',
+                path: 'https://test1.com/1',
+                createdAt: '2024-11-24T01:00:00.000Z',
+                thumbnail: 'https://test1.com/test.png',
+                viewCount: 0,
+              },
+              {
+                id: 2,
+                author: '블로그 이름',
+                title: '게시글 제목',
+                path: 'https://test2.com/1',
+                createdAt: '2024-11-24T02:00:00.000Z',
+                thumbnail: 'https://test2.com/test.png',
+                viewCount: 0,
+              },
+            ],
+          },
+        },
+        continue: {
+          summary: '새로운 트렌드 피드 수신 완료',
+          value: {
+            message: '새로운 트렌드 피드 수신 완료',
+            data: [
+              {
+                id: 3,
+                author: '블로그 이름',
+                title: '게시글 제목',
+                path: 'https://test3.com/1',
+                createdAt: '2024-11-24T03:00:00.000Z',
+                thumbnail: 'https://test3.com/test.png',
+                viewCount: 0,
+              },
+              {
+                id: 4,
+                author: '블로그 이름',
+                title: '게시글 제목',
+                path: 'https://test4.com/1',
+                createdAt: '2024-11-24T04:00:00.000Z',
+                thumbnail: 'https://test4.com/test.png',
+                viewCount: 0,
+              },
+            ],
+          },
         },
       },
     }),
