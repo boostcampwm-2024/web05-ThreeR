eeaa8aea1e4d476923b6abac310f8b7af74b41db
정명기 <jmk101711@yu.ac.kr>
2024-11-26 00:05:38 +0900

    ✨ feat: 관리자 페이지 거절 승인 여부 확인 기능 추가

--- client/src/api/services/admin/rss.ts
+++ client/src/api/services/admin/rss.ts
@@ -6,6 +6,15 @@ export const admin = {
     const response = await axiosInstance.get("/api/rss");
     return response.data;
   },
+  getAccept: async (): Promise<AdminRss> => {
+    const response = await axiosInstance.get("/api/rss/history/accept");
+    return response.data;
+  },
+  getReject: async (): Promise<AdminRss> => {
+    const response = await axiosInstance.get("/api/rss/history/reject");
+    return response.data;
+  },
+
   acceptRss: async (data: AdminRequest): Promise<AdminResponse> => {
     const response = await axiosInstance.post(`/api/rss/accept/${data.id}`);
     return response.data;
--- client/src/components/admin/layout/AdminTabs.tsx
+++ client/src/components/admin/layout/AdminTabs.tsx
@@ -7,7 +7,7 @@ import { RssRequestCard } from "@/components/admin/rss/RssRequestCard";
 import { Badge } from "@/components/ui/badge";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 
-import { useFetchRss } from "@/hooks/queries/useFetchRss";
+import { useFetchRss, useFetchAccept, useFetchReject } from "@/hooks/queries/useFetchRss";
 import { useAdminAccept, useAdminReject } from "@/hooks/queries/useRssActions";
 
 import { AdminRequest } from "@/types/rss";
@@ -16,12 +16,34 @@ type SelectedBlogType = {
   blogName: string;
   blogId: number;
 };
+
 export const AdminTabs = () => {
   const [selectedBlog, setSelectedBlog] = useState<SelectedBlogType>({ blogId: 0, blogName: "" });
-  const { data, isLoading, error, refetchRss } = useFetchRss();
+  const {
+    data: pendingData,
+    isLoading: isPendingLoading,
+    error: pendingError,
+    refetchData: refetchRss,
+  } = useFetchRss();
+
+  const {
+    data: acceptedData,
+    isLoading: isAcceptedLoading,
+    error: acceptedError,
+    refetchData: refetchAccept,
+  } = useFetchAccept();
+
+  const {
+    data: rejectedData,
+    isLoading: isRejectedLoading,
+    error: rejectedError,
+    refetchData: refetchReject,
+  } = useFetchReject();
 
   const onSuccess = () => {
     refetchRss();
+    refetchAccept();
+    refetchReject();
   };
 
   const onError = (error: unknown) => {
@@ -33,6 +55,8 @@ export const AdminTabs = () => {
       console.error("Unknown Error:", error);
     }
     refetchRss();
+    refetchAccept();
+    refetchReject();
   };
 
   const { mutate: acceptMutate } = useAdminAccept(onSuccess, onError);
@@ -46,27 +70,65 @@ export const AdminTabs = () => {
     setSelectedBlog({ blogName, blogId });
   };
 
-  if (isLoading || !data) return <div>Loading...</div>;
-  if (error) return <div>Error loading data</div>;
+  if (isPendingLoading || isAcceptedLoading || isRejectedLoading) return <div>Loading...</div>;
+  if (pendingError || acceptedError || rejectedError) return <div>Error loading data</div>;
 
-  const pendingRss = data?.data;
-
+  const pendingRss = pendingData.data;
+  const acceptedRss = acceptedData.data;
+  const rejectedRss = rejectedData.data;
+  console.log(pendingData);
   return (
     <div>
       <Tabs defaultValue="pending" className="mb-8">
-        <TabsList className="grid w-full grid-cols-3 lg:w-auto">
-          <TabsTrigger value="pending" className="relative">
+        <TabsList className="flex w-full lg:w-auto items-center">
+          <TabsTrigger value="pending" className="relative w-full">
             대기 중
             <Badge variant="secondary" className="ml-2">
               {pendingRss.length}
             </Badge>
           </TabsTrigger>
-          <TabsTrigger value="approved">승인됨</TabsTrigger>
-          <TabsTrigger value="rejected">거부됨</TabsTrigger>
+          <TabsTrigger value="accepted" className="w-full">
+            승인됨
+          </TabsTrigger>
+          <TabsTrigger value="rejected" className="w-full">
+            거부됨
+          </TabsTrigger>
         </TabsList>
 
         <TabsContent value="pending" className="space-y-4 mt-4">
           {pendingRss.map((request) => (
+            <RssRequestCard
+              key={request.id}
+              request={request}
+              onApprove={() => handleActions(request, "accept")}
+              onReject={() =>
+                handleSelectedBlog({
+                  blogName: request.name,
+                  blogId: request.id,
+                })
+              }
+            />
+          ))}
+        </TabsContent>
+
+        <TabsContent value="accepted" className="space-y-4 mt-4">
+          {acceptedRss.map((request) => (
+            <RssRequestCard
+              key={request.id}
+              request={request}
+              onApprove={() => handleActions(request, "accept")}
+              onReject={() =>
+                handleSelectedBlog({
+                  blogName: request.name,
+                  blogId: request.id,
+                })
+              }
+            />
+          ))}
+        </TabsContent>
+
+        <TabsContent value="rejected" className="space-y-4 mt-4">
+          {rejectedRss.map((request) => (
             <RssRequestCard
               key={request.id}
               request={request}
