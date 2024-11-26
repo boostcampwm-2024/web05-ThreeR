bddd989394cb39f9b33e12fb0ecf8c37c44882fa
J_Coder <seok3765@naver.com>
2024-11-26 13:55:29 +0900

    📝 docs: swagger API 작성

---
+++ server/src/statistic/statistic.api-docs.ts
@@ -0,0 +1,68 @@
+import { applyDecorators } from '@nestjs/common';
+import {
+  ApiOkResponse,
+  ApiQuery,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+
+export function ApiTodayStatistic() {
+  return applyDecorators(
+    ApiQuery({
+      name: 'limit',
+      required: false,
+      type: Number,
+      description: '가지고 올 게시글 수',
+      example: 10,
+    }),
+    ApiOkResponse({
+      description: 'Ok',
+      schema: {
+        properties: {
+          message: {
+            type: 'string',
+          },
+          data: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                id: {
+                  type: 'number',
+                },
+                title: {
+                  type: 'string',
+                },
+                viewCount: {
+                  type: 'number',
+                },
+              },
+            },
+          },
+        },
+      },
+      example: {
+        message: '금일 조회수 통계 조회 완료',
+        data: [
+          {
+            id: 1,
+            title: 'testTitle',
+            viewCount: 0,
+          },
+        ],
+      },
+    }),
+    ApiUnauthorizedResponse({
+      description: 'Unauthorized',
+      schema: {
+        properties: {
+          message: {
+            type: 'string',
+          },
+        },
+      },
+      example: {
+        message: '인증되지 않은 요청입니다.',
+      },
+    }),
+  );
+}
