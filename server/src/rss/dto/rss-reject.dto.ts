db00331588414be519e9f2878d9858a15876fc02
asn6878 <asn6878@gmail.com>
2024-11-27 23:51:02 +0900

    ✅ test: rss 거절 e2e 테스트 작성

--- server/src/rss/dto/rss-reject.dto.ts
+++ server/src/rss/dto/rss-reject.dto.ts
@@ -9,4 +9,8 @@ export class RejectRssDto {
   @IsNotEmpty({ message: '거부 사유를 필수로 입력해야 합니다.' })
   @IsString({ message: '거부 사유를 문자열로 작성해주세요.' })
   description: string;
+
+  constructor(description: string) {
+    this.description = description;
+  }
 }
---
+++ server/test/rss/reject.e2e-spec.ts
@@ -0,0 +1,103 @@
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { Repository } from 'typeorm';
+import { Rss, RssReject } from '../../src/rss/rss.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { RssFixture } from './fixture/rssFixture';
+import { RedisService } from '../../src/common/redis/redis.service';
+import { RejectRssDto } from '../../src/rss/dto/rss-reject.dto';
+
+describe('Rss Reject E2E Test', () => {
+  let app: INestApplication;
+  let rssRepository: Repository<Rss>;
+  let rssRejectRepository: Repository<RssReject>;
+  let redisService: RedisService;
+
+  beforeAll(async () => {
+    app = global.testApp;
+    rssRepository = app.get<Repository<Rss>>(getRepositoryToken(Rss));
+    rssRejectRepository = app.get<Repository<RssReject>>(
+      getRepositoryToken(RssReject),
+    );
+    redisService = app.get(RedisService);
+  });
+
+  beforeEach(async () => {
+    await rssRepository.query('DELETE FROM rss');
+    await redisService.redisClient.set('auth:sid', 'test_admin');
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('POST /api/rss/reject/{rssId}', () => {
+    describe('정상적인 요청을 한다.', () => {
+      it('정삳적으로 RSS를 거절한다.', async () => {
+        // given
+        const REJECT_REASON = '거절 사유';
+        const rssFixture = RssFixture.createRssFixture();
+        const rss = await rssRepository.save(rssFixture);
+        const rejectRssDto = new RejectRssDto(REJECT_REASON);
+
+        // when
+        const response = await request(app.getHttpServer())
+          .post(`/api/rss/reject/${rss.id}`)
+          .set('Cookie', 'sessionId=sid')
+          .send(rejectRssDto);
+
+        const accepted = await rssRejectRepository.findOne({
+          where: { description: REJECT_REASON },
+        });
+
+        // then
+        expect(response.status).toBe(201);
+        expect(accepted).not.toBeNull();
+      });
+    });
+
+    describe('비정상적인 요청을 한다.', () => {
+      it('PathVariable이 적절하지 않을 경우', async () => {
+        // when
+        const response = await request(app.getHttpServer())
+          .post(`/api/rss/reject/abc`)
+          .set('Cookie', 'sessionId=sid')
+          .send();
+
+        // then
+        expect(response.status).toBe(400);
+      });
+
+      it('존재하지 않는 rss를 거절할 때', async () => {
+        // given
+        const REJECT_REASON = '거절 사유';
+        const rejectRssDto = new RejectRssDto(REJECT_REASON);
+
+        // when
+        const response = await request(app.getHttpServer())
+          .post(`/api/rss/reject/1`)
+          .set('Cookie', 'sessionId=sid')
+          .send(rejectRssDto);
+
+        // then
+        expect(response.status).toBe(404);
+      });
+
+      it('유효한 세션이 존재하지 않을 때', async () => {
+        // when
+        const noCookieResponse = await request(app.getHttpServer())
+          .post(`/api/rss/reject/1`)
+          .send();
+
+        const noSessionResponse = await request(app.getHttpServer())
+          .post(`/api/rss/reject/1`)
+          .set('Cookie', 'sessionId=invalid')
+          .send();
+
+        // then
+        expect(noCookieResponse.status).toBe(401);
+        expect(noSessionResponse.status).toBe(401);
+      });
+    });
+  });
+});
