ed35c0cea10685609b44d9971a650863d51b138a
codeVac513 <xssdsw@naver.com>
2024-11-29 00:20:21 +0900

    📝 docs: 최신 피드 업데이트 API swagger 수정

--- server/src/feed/feed.api-docs.ts
+++ server/src/feed/feed.api-docs.ts
@@ -300,24 +300,19 @@ export function ApiGetRecentFeedList() {
         },
       },
       example: {
-        connect: {
-          summary: '최신 피드 업데이트 완료',
-          value: {
-            message: '최신 피드 업데이트 완료',
-            data: [
-              {
-                id: 1,
-                author: '블로그 이름',
-                blogPlatform: 'etc',
-                title: '게시글 제목',
-                path: 'https://test1.com/1',
-                createdAt: '2024-11-24T01:00:00.000Z',
-                thumbnail: 'https://test1.com/test.png',
-                viewCount: 0,
-              },
-            ],
+        message: '최신 피드 업데이트 완료',
+        data: [
+          {
+            id: 1,
+            author: '블로그 이름',
+            blogPlatform: 'etc',
+            title: '게시글 제목',
+            path: 'https://test1.com/1',
+            createdAt: '2024-11-24T01:00:00.000Z',
+            thumbnail: 'https://test1.com/test.png',
+            viewCount: 0,
           },
-        },
+        ],
       },
     }),
   );
