e70540184da7fbbc2b1b56ecf98725f9c6773e65
J_Coder <seok3765@naver.com>
2024-12-02 01:58:21 +0900

    ✅ test: rss Reject 코드 일관성을 위해 동적 픽스처로 변경

--- server/test/fixture/rssReject.fixture.ts
+++ server/test/fixture/rssReject.fixture.ts
@@ -3,14 +3,15 @@ import { RssReject } from '../../src/rss/rss.entity';
 export class RssRejectFixture {
   static createRssRejectFixture(
     overwrites: Partial<RssReject> = {},
+    index: number = 1,
   ): RssReject {
     const rssReject = new RssReject();
     Object.assign(rssReject, {
-      name: `blog`,
-      userName: `name`,
-      email: `test@test.com`,
-      rssUrl: `https://example.com/rss`,
-      description: 'description',
+      name: `blog${index}`,
+      userName: `name${index}`,
+      email: `test${index}@test.com`,
+      rssUrl: `https://example${index}.com/rss`,
+      description: `description${index}`,
     });
     return Object.assign(rssReject, overwrites);
   }
