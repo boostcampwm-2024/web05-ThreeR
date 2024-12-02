bc961f5e0485b1007ac06c4e75179543f159cea7
J_Coder <seok3765@naver.com>
2024-12-03 01:23:27 +0900

    ✅ test: dto 테스트 분리, 테스트 데이터 개수, 범위 축소

--- server/test/statistic/all.e2e-spec.ts
+++ server/test/statistic/all.e2e-spec.ts
@@ -1,8 +1,10 @@
 import * as request from 'supertest';
 import { INestApplication } from '@nestjs/common';
-import { RedisService } from '../../src/common/redis/redis.service';
 import { RssAcceptRepository } from '../../src/rss/rss.repository';
 import { FeedRepository } from '../../src/feed/feed.repository';
+import { RssAcceptFixture } from '../fixture/rssAccept.fixture';
+import { FeedFixture } from '../fixture/feed.fixture';
+import { Feed } from '../../src/feed/feed.entity';
 
 describe('All view count statistic E2E Test : GET /api/statistic/all', () => {
   let app: INestApplication;
@@ -10,145 +12,34 @@ describe('All view count statistic E2E Test : GET /api/statistic/all', () => {
     app = global.testApp;
     const rssAcceptRepository = app.get(RssAcceptRepository);
     const feedRepository = app.get(FeedRepository);
-    const redisService = app.get(RedisService);
     const [blog] = await Promise.all([
-      rssAcceptRepository.save({
-        id: 1,
-        name: 'test',
-        userName: 'test',
-        email: 'test@test.com',
-        rssUrl: 'https://test.com/rss',
-      }),
-      redisService.redisClient.set('auth:test1234', 'test'),
+      rssAcceptRepository.save(RssAcceptFixture.createRssAcceptFixture()),
     ]);
-    await feedRepository.save([
-      {
-        id: 1,
-        createdAt: '2024-11-26 09:00:00',
-        title: 'test1',
-        path: 'test1',
-        viewCount: 5,
-        thumbnail: 'https://test.com/test.png',
-        blog: blog,
-      },
-      {
-        id: 2,
-        createdAt: '2024-11-26 09:00:00',
-        title: 'test2',
-        path: 'test2',
-        viewCount: 4,
-        thumbnail: 'https://test.com/test.png',
-        blog: blog,
-      },
-      {
-        id: 3,
-        createdAt: '2024-11-26 09:00:00',
-        title: 'test3',
-        path: 'test3',
-        viewCount: 3,
-        thumbnail: 'https://test.com/test.png',
-        blog: blog,
-      },
-      {
-        id: 4,
-        createdAt: '2024-11-26 09:00:00',
-        title: 'test4',
-        path: 'test4',
-        viewCount: 2,
-        thumbnail: 'https://test.com/test.png',
-        blog: blog,
-      },
-      {
-        id: 5,
-        createdAt: '2024-11-26 09:00:00',
-        title: 'test5',
-        path: 'test5',
-        viewCount: 1,
-        thumbnail: 'https://test.com/test.png',
-        blog: blog,
-      },
-    ]);
+    const feeds: Feed[] = [];
+    for (let i = 1; i <= 2; i++) {
+      feeds.push(FeedFixture.createFeedFixture(blog, { viewCount: i - 1 }, i));
+    }
+    await feedRepository.save(feeds);
   });
 
-  describe('limit 값을 올바르게 입력하지 않았을 경우', () => {
-    it('실수를 입력한다.', async () => {
-      const response = await request(app.getHttpServer())
-        .get('/api/statistic/all?limit=1.1')
-        .set('Cookie', 'sessionId=test1234');
-      expect(response.status).toBe(400);
-      expect(response.body.message).toBe('정수로 입력해주세요.');
-    });
-    it('문자열을 입력한다.', async () => {
-      const response = await request(app.getHttpServer())
-        .get('/api/statistic/all?limit=test')
-        .set('Cookie', 'sessionId=test1234');
-      expect(response.status).toBe(400);
-      expect(response.body.message).toBe('정수로 입력해주세요.');
-    });
+  it('값을 입력 하지 않는다.', async () => {
+    // when
+    const response = await request(app.getHttpServer()).get(
+      '/api/statistic/all',
+    );
 
-    it('음수를 입력한다.', async () => {
-      const response = await request(app.getHttpServer())
-        .get('/api/statistic/all?limit=-100')
-        .set('Cookie', 'sessionId=test1234');
-      expect(response.status).toBe(400);
-      expect(response.body).toStrictEqual({
-        message: 'limit 값은 1 이상이어야 합니다.',
-      });
-    });
+    // then
+    expect(response.status).toBe(200);
+    expect(response.body.data.map((item) => item.id)).toStrictEqual([2, 1]);
   });
+  it('양수를 입력한다.', async () => {
+    // when
+    const response = await request(app.getHttpServer()).get(
+      '/api/statistic/all?limit=1',
+    );
 
-  describe('limit 값을 올바르게 입력했을 경우', () => {
-    it('값을 입력 하지 않는다.', async () => {
-      const response = await request(app.getHttpServer())
-        .get('/api/statistic/all')
-        .set('Cookie', 'sessionId=test1234');
-      expect(response.status).toBe(200);
-      expect(response.body).toStrictEqual({
-        message: '전체 조회수 통계 조회 완료',
-        data: [
-          {
-            id: 1,
-            title: 'test1',
-            viewCount: 5,
-          },
-          {
-            id: 2,
-            title: 'test2',
-            viewCount: 4,
-          },
-          {
-            id: 3,
-            title: 'test3',
-            viewCount: 3,
-          },
-          {
-            id: 4,
-            title: 'test4',
-            viewCount: 2,
-          },
-          {
-            id: 5,
-            title: 'test5',
-            viewCount: 1,
-          },
-        ],
-      });
-    });
-    it('양수를 입력한다.', async () => {
-      const response = await request(app.getHttpServer())
-        .get('/api/statistic/all?limit=1')
-        .set('Cookie', 'sessionId=test1234');
-      expect(response.status).toBe(200);
-      expect(response.body).toStrictEqual({
-        message: '전체 조회수 통계 조회 완료',
-        data: [
-          {
-            id: 1,
-            title: 'test1',
-            viewCount: 5,
-          },
-        ],
-      });
-    });
+    // then
+    expect(response.status).toBe(200);
+    expect(response.body.data.map((item) => item.id)).toStrictEqual([2]);
   });
 });
