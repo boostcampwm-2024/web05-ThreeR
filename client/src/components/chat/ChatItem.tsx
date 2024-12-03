62e9676cbe1cff72d0b9d3252b145675a30b7f47
jungmyunggi <143400940+jungmyunggi@users.noreply.github.com>
2024-12-03 19:01:49 +0900

    💄 style: 채팅 최소 크기 삭제

--- client/src/components/chat/ChatItem.tsx
+++ client/src/components/chat/ChatItem.tsx
@@ -10,8 +10,7 @@ type ChatItemProps = {
   chatItem: ChatType;
   isSameUser: Boolean;
 };
-const chatStyle =
-  "p-3 bg-gray-200 text-black break-words whitespace-pre-wrap rounded-md inline-block max-w-[90%] min-w-[10%]";
+const chatStyle = "p-3 bg-gray-200 text-black break-words whitespace-pre-wrap rounded-md inline-block max-w-[90%]";
 export default function ChatItem({ chatItem, isSameUser }: ChatItemProps) {
   return (
     <div className="flex flex-col ">
