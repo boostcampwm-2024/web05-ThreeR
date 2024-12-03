cd84dd9dee5748a4d7d40cf469c856cb6d63420b
codeVac513 <xssdsw@naver.com>
2024-12-03 18:09:17 +0900

    ✅ test: admin 로그인, 회원가입, sessionId API 테스트 코드 작성

---
+++ server/test/admin/login.e2e-spec.ts
@@ -0,0 +1,65 @@
+import { INestApplication } from '@nestjs/common';
+import { AdminService } from '../../src/admin/admin.service';
+import { LoginAdminDto } from '../../src/admin/dto/login-admin.dto';
+import * as request from 'supertest';
+import { RegisterAdminDto } from '../../src/admin/dto/register-admin.dto';
+describe('POST api/admin/login E2E Test', () => {
+  let app: INestApplication;
+  let adminService: AdminService;
+  const registerAdminDto: RegisterAdminDto = {
+    loginId: 'testAdminId',
+    password: 'testAdminPassword!',
+  };
+
+  beforeAll(async () => {
+    app = global.testApp;
+    adminService = app.get(AdminService);
+    await adminService.registerAdmin(registerAdminDto);
+  });
+  it('등록된 계정이면 정상적으로 로그인할 수 있다.', async () => {
+    //given
+    const loginAdminDto: LoginAdminDto = {
+      loginId: 'testAdminId',
+      password: 'testAdminPassword!',
+    };
+
+    //when
+    const response = await request(app.getHttpServer())
+      .post('/api/admin/login')
+      .send(loginAdminDto);
+
+    //then
+    expect(response.status).toBe(200);
+  });
+
+  it('등록되지 않은 ID로 로그인을 시도하면 401 UnAuthorized 예외가 발생한다.', async () => {
+    //given
+    const loginWrongAdminIdDto: LoginAdminDto = {
+      loginId: 'testWrongAdminId',
+      password: 'testAdminPassword!',
+    };
+
+    //when
+    const response = await request(app.getHttpServer())
+      .post('/api/admin/login')
+      .send(loginWrongAdminIdDto);
+
+    //then
+    expect(response.status).toBe(401);
+  });
+
+  it('비밀번호가 다르다면 401 UnAuthorized 예외가 발생한다.', async () => {
+    //given
+    const loginWrongAdminPasswordDto: LoginAdminDto = {
+      loginId: 'testAdminId',
+      password: 'testWrongAdminPassword!',
+    };
+    //when
+    const response = await request(app.getHttpServer())
+      .post('/api/admin/login')
+      .send(loginWrongAdminPasswordDto);
+
+    //then
+    expect(response.status).toBe(401);
+  });
+});
---
+++ server/test/admin/register.e2e.spect.ts
@@ -0,0 +1,73 @@
+import { INestApplication } from '@nestjs/common';
+import { AdminService } from '../../src/admin/admin.service';
+import { LoginAdminDto } from '../../src/admin/dto/login-admin.dto';
+import { RegisterAdminDto } from '../../src/admin/dto/register-admin.dto';
+import * as request from 'supertest';
+
+describe('POST api/admin/register E2E Test', () => {
+  let app: INestApplication;
+  let adminService: AdminService;
+
+  //given
+  const loginAdminDto: LoginAdminDto = {
+    loginId: 'testAdminId',
+    password: 'testAdminPassword!',
+  };
+  const registerAdminDto: RegisterAdminDto = {
+    loginId: 'testNewAdminId',
+    password: 'testNewAdminPassword!',
+  };
+
+  beforeAll(async () => {
+    app = global.testApp;
+    adminService = app.get(AdminService);
+
+    await adminService.registerAdmin(loginAdminDto);
+  });
+
+  it('관리자가 로그인되어 있으면 다른 관리자 계정 회원가입을 할 수 있다.', async () => {
+    //given
+    const agent = request.agent(app.getHttpServer());
+
+    //when
+    await agent.post('/api/admin/login').send(loginAdminDto);
+    const response = await agent
+      .post('/api/admin/register')
+      .send(registerAdminDto);
+
+    //then
+    expect(response.status).toBe(201);
+  });
+
+  it('이미 가입한 ID를 입력하면 관리자 계정을 생성할 수 없다.', async () => {
+    //given
+    const agent = request.agent(app.getHttpServer());
+
+    //when
+    await agent.post('/api/admin/login').send(loginAdminDto);
+    const response = await agent
+      .post('/api/admin/register')
+      .send(registerAdminDto);
+
+    //then
+    expect(response.status).toBe(409);
+  });
+
+  it('관리자가 로그아웃 상태면 401 UnAuthorized 예외가 발생한다.', async () => {
+    //given
+    const registerAdminDto: RegisterAdminDto = {
+      loginId: 'testNewAdminId',
+      password: 'testNewAdminPassword!',
+    };
+
+    const agent = request.agent(app.getHttpServer());
+
+    //when
+    const response = await agent
+      .post('/api/admin/register')
+      .send(registerAdminDto);
+
+    //then
+    expect(response.status).toBe(401);
+  });
+});
