1efad8267f79e7688601b2269b8c0b02735c37f9
asn6878 <asn6878@gmail.com>
2024-12-03 19:23:38 +0900

    ♻️ refactor: 스웨거 경로 변경

--- server/src/common/swagger/swagger.ts
+++ server/src/common/swagger/swagger.ts
@@ -9,5 +9,5 @@ export function setupSwagger(app: INestApplication) {
     .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, documentFactory);
+  SwaggerModule.setup('api/swagger', app, documentFactory);
 }
