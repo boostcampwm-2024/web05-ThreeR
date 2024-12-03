3ab5388d53e72d0dbd33ed9053e93cdff3d06ee0
codeVac513 <xssdsw@naver.com>
2024-12-04 03:54:33 +0900

    ✅ test: rssFixture에서 RssAcceptFixture 사용으로 수정

--- server/test/feed/feedId.e2e-spec.ts
+++ server/test/feed/feedId.e2e-spec.ts
@@ -5,7 +5,7 @@ import { RedisService } from '../../src/common/redis/redis.service';
 import { FeedRepository } from '../../src/feed/feed.repository';
 import { RssAcceptRepository } from '../../src/rss/rss.repository';
 import { FeedFixture } from '../fixture/feed.fixture';
-import { RssFixture } from '../fixture/rss.fixture';
+import { RssAcceptFixture } from '../fixture/rssAccept.fixture';
 
 describe('POST /api/feed/:feedId E2E Test', () => {
   let app: INestApplication;
@@ -20,7 +20,9 @@ describe('POST /api/feed/:feedId E2E Test', () => {
     const feedRepository = app.get(FeedRepository);
     const rssAcceptRepository = app.get(RssAcceptRepository);
 
-    const blog = await rssAcceptRepository.save(RssFixture.createRssFixture());
+    const blog = await rssAcceptRepository.save(
+      RssAcceptFixture.createRssAcceptFixture(),
+    );
 
     const feeds = Array.from({ length: latestId }).map((_, i) => {
       return FeedFixture.createFeedFixture(blog, _, i + 1);
