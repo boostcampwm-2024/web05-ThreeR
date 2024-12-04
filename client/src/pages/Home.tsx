fd8a608dc9022f9879b2eb48577c8a40486d1478
junyeokk <cjh4302@gmail.com>
2024-12-04 22:07:01 +0900

    ♻️ refactor: 차트 컴포넌트 lazy loading 구현
    - 차트 laxy loading을 위한 ChartTab 컴포넌트 생성
    - Chart 컴포넌트 구조 개선
    - Home 컴포넌트에 lazy loading 적용

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,24 @@
-import Chart from "@/components/chart/Chart";
+import { lazy, Suspense } from "react";
+
+import ChartSkeleton from "@/components/chart/ChartSkeleton.tsx";
 import Layout from "@/components/layout/Layout";
 import MainContent from "@/components/sections/MainContent";
 
 import { useTapStore } from "@/store/useTapStore";
+
+const ChartTab = lazy(() => import("@/components/chart/ChartTab"));
 
 export default function Home() {
   const { tap } = useTapStore();
 
   const renderFunction = () => {
     if (tap === "main") return <MainContent />;
-    return <Chart />;
+    return (
+      <Suspense fallback={<ChartSkeleton />}>
+        <ChartTab />)
+      </Suspense>
+    );
   };
+
   return <Layout>{renderFunction()}</Layout>;
 }
