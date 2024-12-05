aa0d0b81e9b8744e719ead13325d16fccb69ea87
junyeokk <cjh4302@gmail.com>
2024-12-05 16:10:54 +0900

    ♻️ refactor: 라우팅에서의 code splitting으로 인한 변경

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,23 +1,15 @@
-import { lazy, Suspense } from "react";
-
-import ChartSkeleton from "@/components/chart/ChartSkeleton.tsx";
+import ChartTab from "@/components/chart/ChartTab.tsx";
 import Layout from "@/components/layout/Layout";
 import MainContent from "@/components/sections/MainContent";
 
 import { useTapStore } from "@/store/useTapStore";
-
-const ChartTab = lazy(() => import("@/components/chart/ChartTab"));
 
 export default function Home() {
   const { tap } = useTapStore();
 
   const renderFunction = () => {
     if (tap === "main") return <MainContent />;
-    return (
-      <Suspense fallback={<ChartSkeleton />}>
-        <ChartTab />)
-      </Suspense>
-    );
+    return <ChartTab />;
   };
 
   return <Layout>{renderFunction()}</Layout>;
