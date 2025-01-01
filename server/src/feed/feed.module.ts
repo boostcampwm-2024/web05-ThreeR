f889181566c7f6c345d5fc2efd70f8e041cf2d3b
J_Coder <seok3765@naver.com>
2025-01-02 01:47:53 +0900

    🧼 clean: 피드 뷰 의존성 추가

--- server/src/feed/feed.module.ts
+++ server/src/feed/feed.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FeedController } from './feed.controller';
 import { FeedService } from './feed.service';
-import { FeedRepository } from './feed.repository';
+import { FeedRepository, FeedViewRepository } from './feed.repository';
 import { ScheduleModule } from '@nestjs/schedule';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [ScheduleModule.forRoot(), EventEmitterModule.forRoot()],
   controllers: [FeedController],
-  providers: [FeedService, FeedRepository],
+  providers: [FeedService, FeedRepository, FeedViewRepository],
 })
 export class FeedModule {}
