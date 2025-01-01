2048a4c6fdfb5a2dd727fb2e23888b9c69084066
J_Coder <seok3765@naver.com>
2025-01-01 22:19:02 +0900

    ♻️ refactor: mysql, redis 접근 코드 클래스화, executeQuery 예외 처리

---
+++ feed-crawler/src/common/mysql-access.ts
@@ -0,0 +1,112 @@
+import * as mysql from "mysql2/promise";
+import { CONNECTION_LIMIT } from "./constant";
+import { PoolConnection } from "mysql2/promise";
+import logger from "./logger";
+import { FeedDetail, RssObj } from "./types";
+
+export class MySQLRepository {
+  private static instance: MySQLRepository;
+  private pool = this.createPool();
+  private nameTag: string = "[MySQL]";
+  private constructor() {}
+
+  static getInstance(): MySQLRepository {
+    if (!MySQLRepository.instance) {
+      MySQLRepository.instance = new MySQLRepository();
+    }
+    return MySQLRepository.instance;
+  }
+
+  private createPool() {
+    return mysql.createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      connectionLimit: CONNECTION_LIMIT,
+    });
+  }
+
+  private async executeQuery<T>(query: string, params: any[] = []) {
+    let connection: PoolConnection;
+    try {
+      connection = await this.pool.getConnection();
+      const [rows] = await connection.query(query, params);
+      return rows as T[];
+    } catch (error) {
+      logger.error(
+        `${this.nameTag} 쿼리 ${query} 실행 중 오류 발생
+        오류 메시지: ${error.message}
+        스택 트레이스: ${error.stack}`
+      );
+    } finally {
+      if (connection) {
+        try {
+          if (connection) connection.release();
+        } catch (error) {
+          logger.error(
+            `${this.nameTag} connection release 중 오류 발생
+            오류 메시지: ${error.message}
+            스택 트레이스: ${error.stack}`
+          );
+        }
+      }
+    }
+  }
+
+  public async selectAllRss(): Promise<RssObj[]> {
+    const query = `SELECT id, rss_url as rssUrl, name as blogName, blog_platform as blogPlatform
+    FROM rss_accept`;
+    return this.executeQuery(query);
+  }
+
+  public async insertFeeds(resultData: FeedDetail[]) {
+    const query = `
+        INSERT INTO feed (blog_id, created_at, title, path, thumbnail)
+        VALUES (?, ?, ?, ?, ?)
+    `;
+
+    const insertPromises = resultData.map(async (feed) => {
+      return this.executeQuery(query, [
+        feed.blogId,
+        feed.pubDate,
+        feed.title,
+        feed.link,
+        feed.imageUrl,
+      ]);
+    });
+
+    const promiseResults = await Promise.allSettled(insertPromises);
+
+    const failedInserts = promiseResults.filter(
+      (result) => result.status === "rejected"
+    );
+    failedInserts.forEach((failed) => {
+      const error = (failed as PromiseRejectedResult).reason;
+      logger.error(
+        `${this.nameTag} 피드 삽입 실패
+        에러 메시지: ${error.message}
+        스택 트레이스: ${error.stack}`
+      );
+    });
+
+    const insertedFeeds = promiseResults
+      .filter((result) => result.status === "fulfilled")
+      .map((result, index) => {
+        const insertId = (result as PromiseFulfilledResult<any>).value.insertId;
+        return {
+          ...resultData[index],
+          id: insertId,
+        };
+      });
+
+    logger.info(
+      `${this.nameTag} ${insertedFeeds.length}개의 피드 데이터가 성공적으로 데이터베이스에 삽입되었습니다.`
+    );
+    return insertedFeeds;
+  }
+
+  public async end() {
+    await this.pool.end();
+  }
+}
---
+++ feed-crawler/src/common/redis-access.ts
@@ -0,0 +1,111 @@
+import Redis from "ioredis";
+import logger from "./logger";
+import { redisConstant } from "./constant";
+import { FeedDetail } from "./types";
+
+export class RedisRepository {
+  private static instance: RedisRepository;
+  private redis: Redis;
+  private nameTag: string = "[Redis]";
+  private constructor() {}
+  private connect() {
+    this.redis = new Redis({
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT),
+      username: process.env.REDIS_USERNAME,
+      password: process.env.REDIS_PASSWORD,
+    });
+  }
+
+  static getInstance() {
+    if (!RedisRepository.instance) {
+      RedisRepository.instance = new RedisRepository();
+    }
+    return RedisRepository.instance;
+  }
+
+  async deleteRecentFeed() {
+    try {
+      this.connect();
+      const keysToDelete = [];
+      let cursor = "0";
+      do {
+        const [newCursor, keys] = await this.redis.scan(
+          cursor,
+          "MATCH",
+          redisConstant.FEED_RECENT_ALL_KEY,
+          "COUNT",
+          "100"
+        );
+        keysToDelete.push(...keys);
+        cursor = newCursor;
+      } while (cursor !== "0");
+
+      if (keysToDelete.length > 0) {
+        await this.redis.del(...keysToDelete);
+      }
+    } catch (error) {
+      logger.error(
+        `${this.nameTag} 최근 게시글 캐시를 삭제하는 도중 에러가 발생했습니다.
+        에러 메시지: ${error.message}
+        스택 트레이스: ${error.stack}`
+      );
+    } finally {
+      if (this.redis) {
+        try {
+          await this.redis.quit();
+        } catch (error) {
+          logger.error(
+            `${this.nameTag} connection quit 중 오류 발생
+            에러 메시지: ${error.message}
+            스택 트레이스: ${error.stack}`
+          );
+        }
+      }
+    }
+    logger.info(
+      `${this.nameTag} 최근 게시글 캐시가 정상적으로 삭제되었습니다.`
+    );
+  }
+
+  async setRecentFeedList(feedLists: FeedDetail[]) {
+    try {
+      this.connect();
+      const pipeLine = this.redis.pipeline();
+      for (const feed of feedLists) {
+        pipeLine.hset(`feed:recent:${feed.id}`, {
+          id: feed.id,
+          blogPlatform: feed.blogPlatform,
+          createdAt: feed.pubDate,
+          viewCount: 0,
+          blogName: feed.blogName,
+          thumbnail: feed.imageUrl,
+          path: feed.link,
+          title: feed.title,
+        });
+      }
+      await pipeLine.exec();
+    } catch (error) {
+      logger.error(
+        `${this.nameTag} 최근 게시글 캐시를 저장하는 도중 에러가 발생했습니다.
+        에러 메시지: ${error.message}
+        스택 트레이스: ${error.stack}`
+      );
+    } finally {
+      if (this.redis) {
+        try {
+          await this.redis.quit();
+        } catch (error) {
+          logger.error(
+            `${this.nameTag} connection quit 중 오류 발생
+                  에러 메시지: ${error.message}
+                  스택 트레이스: ${error.stack}`
+          );
+        }
+      }
+    }
+    logger.info(
+      `${this.nameTag} 최근 게시글 캐시가 정상적으로 저장되었습니다.`
+    );
+  }
+}
