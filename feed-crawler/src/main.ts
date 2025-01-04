2f24fefe44cdaddf254bb1d13a6717da3888e51b
asn6878 <asn6878@gmail.com>
2025-01-04 16:46:37 +0900

    🐛 fix: import 및 경로 오탈자 수정

--- feed-crawler/src/main.ts
+++ feed-crawler/src/main.ts
@@ -1,6 +1,6 @@
 import logger from "./common/logger.js";
 import { FeedCrawler } from "./feed-crawler.js";
-import { MySQLRepository } from "./common/mysql-access.js";
+import { mysqlConnection } from "./common/mysql-access.js";
 
 async function runCrawler() {
   logger.info("==========작업 시작==========");
@@ -9,7 +9,7 @@ async function runCrawler() {
   await feedCrawler.start();
   const endTime = Date.now();
   const executionTime = endTime - startTime;
-  await MySQLRepository.getInstance().end();
+  await mysqlConnection.end();
   logger.info(`실행 시간: ${executionTime / 1000}seconds`);
   logger.info("==========작업 완료==========");
 }
