07098aec2a022c13f08b5808375fd74198ac8002
J_Coder <seok3765@naver.com>
2025-01-04 22:29:54 +0900

    🧼 clean: feed-crawler 변수명, 함수명 가독성 향상

--- feed-crawler/src/main.ts
+++ feed-crawler/src/main.ts
@@ -1,11 +1,10 @@
 import logger from "./common/logger.js";
-import { FeedCrawler } from "./feed-crawler.js";
+import { feedCrawler } from "./feed-crawler.js";
 import { mysqlConnection } from "./common/mysql-access.js";
 
 async function runCrawler() {
   logger.info("==========작업 시작==========");
   const startTime = Date.now();
-  const feedCrawler = new FeedCrawler();
   await feedCrawler.start();
   const endTime = Date.now();
   const executionTime = endTime - startTime;
